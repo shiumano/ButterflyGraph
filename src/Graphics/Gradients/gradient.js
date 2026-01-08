27e22688db53247d11766b3ba4248bea72d07ea1
Shiumano <takkunmail2005@gmail.com>
2026-01-08 08:08:22 +0000

    Gradientを追加 DrawObjectの世界でctxを触ることができない以上こんなオブジェクトを作るしか無い

---
+++ src/Graphics/Gradients/gradient.js
@@ -0,0 +1,133 @@
+/**
+ * @typedef {{ position: number, color: string }} ColorStop
+ */
+
+export class Gradient {
+    /** @type {ColorStop[]} */
+    #colorStops = [];
+    /** @type {GradientBuilder?} */
+    #builderCance = null;
+    #gradientChanged = true;
+
+    /**
+     * @param {ColorStop[]} stops 
+     */
+    constructor(stops = []) {
+        this.#colorStops = stops;
+    }
+
+    /**
+     * @param {number} position 0〜1
+     * @param {string} color CSS color
+     */
+    addColorStop(position, color) {
+        this.#colorStops.push({ position, color });
+        this.requestRecreate();
+    }
+
+    getColorStops() {
+        return Object.freeze(this.#colorStops);
+    }
+
+    clearColorStops() {
+        this.#colorStops = [];
+        this.requestRecreate();
+    }
+
+    requestRecreate() {
+        this.#gradientChanged = true;
+    }
+
+    createGradientBuilder() {
+        return new GradientBuilder(this.getColorStops());
+    }
+
+    getGradientBuilder() {
+        let builder = this.#builderCance;
+
+        if (this.#gradientChanged || builder === null) {
+            builder = this.createGradientBuilder();
+            this.#builderCance = builder;
+        }
+
+        return builder;
+    }
+}
+
+export class GradientBuilder {
+    /** @type {readonly ColorStop[]} */
+    #colorStops = [];
+    /** @type {Map<number, CanvasGradient | string>} */
+    cache = new Map();
+
+    /**
+     * @param {readonly ColorStop[]} stops 
+     */
+    constructor(stops) {
+        this.#colorStops = stops;
+    }
+
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} ctx 
+     */
+    getGradient(ctx) {
+        const id = getContextId(ctx);
+
+        // 同じctxの場合：作り直すのは無駄なので再利用
+        // 違うctxの場合：使い回せないので再作成
+        let gradient = this.cache.get(id);
+
+        if (this._updateCache || !gradient) {
+            // --- Gradient を作る ---
+            gradient = this.createGradient(ctx);
+
+            // キャッシュに保存
+            this.cache.set(id, gradient);
+
+            this._updateCache = false;
+        }
+
+        return gradient;
+    }
+
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} ctx
+     * @returns {CanvasGradient | string}
+     */
+    createGradient(ctx) {
+        throw new Error("Not implemented");
+    }
+
+    /**
+     * @protected
+     * @param {CanvasGradient} grad
+     */
+    _applyStops(grad) {
+        for (let i = 0; i < this.#colorStops.length; i++) {
+            const cs = this.#colorStops[i];
+            grad.addColorStop(cs.position, cs.color);
+        }
+        return grad;
+    }
+}
+
+/** @type {WeakMap<CanvasRenderingContext2D, number>} */
+const ctxIdMap = new WeakMap();
+let ctxIdCounter = 0;
+
+/**
+ * 
+ * @param {CanvasRenderingContext2D} ctx 
+ */
+function getContextId(ctx) {
+    let ctxId = ctxIdMap.get(ctx);
+    if (ctxId === undefined) {
+        ctxId = ctxIdCounter++;
+        ctxIdMap.set(ctx, ctxId);
+    }
+
+    return ctxId;
+}
+
---
+++ src/Graphics/Gradients/linearGradient.js
@@ -0,0 +1,89 @@
+import { Gradient, GradientBuilder } from "./gradient.js";
+
+/**
+ * @import { ColorStop } from "./gradient.js";
+ */
+
+export class LinearGradient extends Gradient {
+    #x0;
+    #y0;
+    #x1;
+    #y1;
+
+    /**
+     * 
+     * @param {number} x0
+     * @param {number} y0
+     * @param {number} x1
+     * @param {number} y1
+     * @param {ColorStop[]} stops
+     */
+    constructor(x0, y0, x1, y1, stops = []) {
+        super(stops);
+        this.#x0 = x0;
+        this.#y0 = y0;
+        this.#x1 = x1;
+        this.#y1 = y1;
+    }
+
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
+    setStart(x, y) {
+        this.#x0 = x;
+        this.#y0 = y;
+        this.requestRecreate();
+    }
+
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
+    setEnd(x, y) {
+        this.#x1 = x;
+        this.#y1 = y;
+        this.requestRecreate();
+    }
+
+    createGradientBuilder() {
+        return new LinearGradientBuilder(this.#x0, this.#y0, this.#x1, this.#y1, this.getColorStops());
+    }
+}
+
+class LinearGradientBuilder extends GradientBuilder {
+    #x0;
+    #y0;
+    #x1;
+    #y1;
+
+    /**
+     * @param {number} x0
+     * @param {number} y0
+     * @param {number} x1
+     * @param {number} y1
+     * @param {readonly ColorStop[]} stops
+     */
+    constructor(x0, y0, x1, y1, stops = []) {
+        super(stops);
+        this.#x0 = x0;
+        this.#y0 = y0;
+        this.#x1 = x1;
+        this.#y1 = y1;
+    }
+
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} ctx 
+     */
+    createGradient(ctx) {
+        const grad = ctx.createLinearGradient(
+            this.#x0, this.#y0,
+            this.#x1, this.#y1
+        );
+
+        return this._applyStops(grad);
+    }
+}
