4960ab8e2cb711e3803d2368d00307621bf32710
Shiumano <takkunmail2005@gmail.com>
2026-01-27 04:07:08 +0000

    AnimationBaseにtimeRangeを追加 drationと別に管理

--- src/Graphics/Animations/animationBase.js
+++ src/Graphics/Animations/animationBase.js
@@ -2,15 +2,16 @@ export class AnimationBase {
     startValue = 0;
     endValue;
     duration;
+    timeRange;
 
     /**
-     *
      * @param {number} end
      * @param {number} duration
      */
     constructor(end, duration) {
         this.endValue = end;
         this.duration = duration;
+        this.timeRange = duration;
     }
 
     /**
@@ -20,7 +21,7 @@ export class AnimationBase {
     getValue(t) {
         const moveRange = this.endValue - this.startValue;
         // duration=0の場合は即座に終了値へ遷移(0/0でNaNが生まれるので)、それ以外は通常の正規化
-        const norm = this.duration === 0 ? 1 : Math.min(Math.max(t / this.duration, 0), 1);
+        const norm = this.timeRange === 0 ? 1 : Math.min(Math.max(t / this.timeRange, 0), 1);
 
         return this.startValue + moveRange * this.leap(norm);
     }
