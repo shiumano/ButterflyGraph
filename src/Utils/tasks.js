6c110c5c8965684aedefa643e2a418e47b912a5f
Shiumano <takkunmail2005@gmail.com>
2026-06-17 02:03:10 +0000

    コメントを追加 恩着せがましく

--- src/Utils/tasks.js
+++ src/Utils/tasks.js
@@ -1,14 +1,39 @@
-// トップレベル以外からの場所から呼び出すとwindowから呼び出されたことにならず壊れることがある
+// トップレベル以外からの場所(とくにPromise)から呼び出すとwindowから呼び出されたことにならず壊れることがある
 // bindしておくことで、どこから呼び出してもwindowから呼び出されたことになる
 const fixedRequestAnimationFrame = requestAnimationFrame.bind(window);
 
-/***
+/**
  * @param {number} ms
  */
 export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/**
+ * 次のフレームが描画されるタイミングで解決されるPromiseを返す
+ * @returns {Promise<number>} 解決された時点のタイムスタンプを返すPromise
+ */
 export function waitVsync() {
+    // つまりこういうこと
+    // return new Promise(resolve => {
+    //     function animationCallback(timestamp) {
+    //         resolve(timestamp);
+    //     }
+    //     requestAnimationFrame(animationCallback);
+    // });
+    //
+    // でもよく見るとanimationCallbackとresolveのシグネチャは同じだから、これで動くはず
+    // return new Promise(resolve => {
+    //     requestAnimationFrame(resolve)
+    // });
+    //
+    // さらにPromiseコンストラクタの引数は”コールバック関数を受け取る関数”で、requestAnimationFrameも同じ
+    // ということでrequestAnimationFrameをそのまま渡してもいけるはず
+    //
+    // つまり、PromiseコンストラクタにrequestAnimationFrameを渡すだけで
+    // 「次のフレームのタイミングで、そのフレームのタイムスタンプで解決されるPromise」
+    // が出来上がる
+    //
+    // それがこれ↓
     return new Promise(fixedRequestAnimationFrame);
 }
