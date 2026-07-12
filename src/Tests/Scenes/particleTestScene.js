16903000bddae70856ee8d3cfb90f66e262e04d2
Shiumano <takkunmail2005@gmail.com>
2026-07-12 20:16:43 +0900

    ParticleTestSceneを調整 プロファイラをみるとそんなに速度は変わらないんだけどね

--- src/Tests/Scenes/particleTestScene.js
+++ src/Tests/Scenes/particleTestScene.js
@@ -7,19 +7,29 @@ import { Vector2 } from "../../Graphics/vector2.js";
 export class ParticleTestScene extends TestScene {
     async load() {
         const particles = [...Array(5000)].map(() => new Circle({ radius: 3 }));
+        const particleAnimations = particles.map(circle => {
+            const r = Math.random() * Math.PI * 2;
+            const d = Math.sqrt(Math.random());
+            const x = Math.sin(r) * d * 1200;
+            const y = Math.cos(r) * d * 1500;
+
+            return circle.addAnimation("pos", (value) => {
+                circle.x = value * x;
+                circle.y = value * y;
+            });
+        });
+
         const gravityContainer = new Container({ anchor: new Vector2(0.5, 0.2), fillStyle: "cyan", children: particles });
+        const gravityAnimation = gravityContainer.animate("y", direct);
 
         this.addButton("Splash", (ev) => {
             const animation_length = 10000;
 
             const now = this.toLocalTime(ev.timeStamp);
-            gravityContainer.animate("y", direct).jump(now).set(0).easeIn(10000, animation_length);
+            gravityAnimation.jump(now).set(0).easeIn(10000, animation_length);
 
-            for (const circle of particles) {
-                const r = Math.random() * Math.PI * 2;
-                const c = Math.sqrt(Math.random());
-                circle.animate("x", direct).jump(now).set(0).to(1200 * Math.sin(r) * c, animation_length);
-                circle.animate("y", direct).jump(now).set(0).to(1500 * Math.cos(r) * c, animation_length);
+            for (const anim of particleAnimations) {
+                anim.jump(now).set(0).to(1, animation_length);
             }
         });
 
