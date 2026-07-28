34d6b77ff8f63e7384d0988c2e06b1b8474fb127
Shiumano <takkunmail2005@gmail.com>
2026-07-28 11:30:28 +0900

    WeakArrayに機能追加

--- src/Utils/memory.js
+++ src/Utils/memory.js
@@ -4,12 +4,57 @@
 export class WeakArray {
     /** @type {WeakRef<T>[]} */
     #weakRefs = [];
+    /** @type {T[]} */
+    #tmpStrongRefs = [];
 
     /**
      * @param {T} obj
      */
     push(obj) {
         this.#weakRefs.push(new WeakRef(obj));
+    }
+
+    // しょうがない: 似たようなコードが大量に出てくるが、WeakRefってのは面倒なものだからこうするしかない
+    /**
+     * @param {T} obj
+     */
+    remove(obj) {
+        const refs = this.#weakRefs;
+        for (let i = 0; i < refs.length; i++) {
+            if (refs[i].deref() === obj) {
+                refs.splice(i, 1);
+                return;
+            }
+        }
+    }
+
+    /**
+     * @param {T} obj
+     */
+    includes(obj) {
+        const stringReference = this.#tmpStrongRefs;
+        this.deref(stringReference);
+
+        const objIncludes = stringReference.includes(obj);
+        stringReference.length = 0;
+
+        return objIncludes;
+    }
+
+    /**
+     * @param {(value: T) => void} callbackFn
+     */
+    forEach(callbackFn) {
+        const refs = this.#weakRefs;
+
+        for (let i = refs.length - 1; i >= 0; i--) {
+            const obj = refs[i].deref();
+            if (obj !== undefined) {
+                callbackFn(obj);
+            } else {
+                refs.splice(i, 1);
+            }
+        }
     }
 
     /**
