63a90e86bb22e679a4c3f74de29ec591c109d76d
Shiumano <takkunmail2005@gmail.com>
2026-07-30 15:00:50 +0900

    childrenをローカル変数に落とす JITが効いたり効かなかったり不安定だったので

--- src/Graphics/Containers/container.js
+++ src/Graphics/Containers/container.js
@@ -189,10 +189,11 @@ export class Container extends DrawObject {
      */
     addChild(...children) {
         this.#frozenChildren = null;
+        const thisChildren = this.#children;
 
         for (let i = 0; i < children.length; i++) {
             const child = children[i];
-            const index = this.#children.indexOf(child);
+            const index = thisChildren.indexOf(child);
             if (index !== -1) continue;  // 既にあるので、追加する意味はない
 
             const oldParent = child.parent;
@@ -202,8 +203,10 @@ export class Container extends DrawObject {
                 oldParent.removeChild(child);  // childを奪う そういう仕様とする
             }
 
-            this.#children.push(child);
-            this.#children.sort((a, b) => a.zIndex - b.zIndex);
+            thisChildren.push(child);
+            if (thisChildren.length > 1 && thisChildren[thisChildren.length - 1].zIndex > child.zIndex) {
+                this.#children.sort((a, b) => a.zIndex - b.zIndex);
+            }
             this.#childrenTimed ||= child.timed;
             this.#childrenAnimated ||= child.animated;
             this.#perfectlyOptimized &&= child.perfectlyOptimized;
@@ -382,12 +385,15 @@ export class ContainerNode extends DrawNode {
      * @param {CanvasRenderingContext2D} ctx
      */
     draw(ctx) {
-        if (this.#clipPath !== null) {
-            ctx.clip(this.#clipPath);
+        const clipPath = this.#clipPath;
+        const children = this.#children;
+
+        if (clipPath !== null) {
+            ctx.clip(clipPath);
         }
 
-        for (let i = 0; i < this.#children.length; i++) {
-            this.#children[i].render(ctx);
+        for (let i = 0; i < children.length; i++) {
+            children[i].render(ctx);
         }
     }
 }
